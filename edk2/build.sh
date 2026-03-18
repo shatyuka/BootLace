583dd4a0e85b267d095281abc92d33b86f8f24f9
shatyuka <shatyuka@qq.com>
2026-03-18 11:36:20 +0800

    fix: BaseTools build failed

--- edk2/build.sh
+++ edk2/build.sh
@@ -2,10 +2,10 @@
 
 COMPILER_PATH=$1
 EDK2_PATH=$2
-export PATH="$COMPILER_PATH:$PATH"
 export EDK_TOOLS_PATH="$EDK2_PATH/BaseTools"
 export PACKAGES_PATH="$EDK2_PATH:$(dirname $(cd $(dirname $0);pwd))"
 . "$EDK2_PATH/edksetup.sh" --reconfig
 make -C "$EDK2_PATH/BaseTools"
 
+export PATH="$COMPILER_PATH:$PATH"
 build -y report.txt -Y COMPILE_INFO -a $3 -t $4 -b $5 -p $6
